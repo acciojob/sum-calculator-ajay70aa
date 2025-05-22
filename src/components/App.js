ae855d75c46c82a77437d78ac99834978174fbd9
Ajay70aa <ajay70aa@gmail.com>
2025-05-22 18:39:16 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [numbers, setNumbers] = useState([])
+  const [sum, setSum] = useState(0)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const sum = numbers.reduce((x, y) =>  x + y, 0);
+      setSum(sum);
+    }, 0)
+    return () => clearTimeout(timeout)
+  }, [numbers])
+
+  function handleChange(e){
+    setNumbers([...numbers, parseInt(e.target.value)])
+  }
   return (
-    <div>
+    <div id="main">
         {/* Do not remove the main div */}
+        <h1 className="heading">Sum Calculator</h1>
+        <input type="number" onChange={(e) => handleChange(e)}/>
+        <p className="result">Sum: {sum}</p>
     </div>
   )
 }
